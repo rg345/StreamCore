12fdc075843f96a8f61117aa52b1958838a6732b
Rohit Gunwal <rgunwal345@gmail.com>
2025-04-01 12:03:00 +0530

    add refresh token functionality and enhance JWT verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -20,6 +20,7 @@ export const verifyJWT = asyncHandler(async (req,res,next) => {
         }
         
         req.user = user;
+        console.log(req.user);
         next()
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Access Token")
